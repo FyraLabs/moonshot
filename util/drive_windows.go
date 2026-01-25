3963ca255f7025a885bf0076289b7a9a86eb5c53
Lleyton Gray <lleyton@fyralabs.com>
2026-01-25 12:53:00 -0800

    *

--- util/drive_windows.go
+++ util/drive_windows.go
@@ -8,18 +8,40 @@ import (
 	"golang.org/x/sys/windows"
 )
 
+const (
+	FSCTL_LOCK_VOLUME         = uint32(0x90018)
+	FSCTL_DISMOUNT_VOLUME     = uint32(0x90020)
+	IOCTL_STORAGE_EJECT_MEDIA = uint32(0x2d4808)
+)
+
 func GetDrivePath(name string) string {
 	return name
 }
 
 func Eject(drivePath string) error {
+	driveFile, err := os.Open(drivePath)
+	if err != nil {
+		return err
+	}
+	defer driveFile.Close()
+
+	handle := windows.Handle(driveFile.Fd())
+
+	var bytesReturned uint32
+	err = windows.DeviceIoControl(
+		handle,
+		IOCTL_STORAGE_EJECT_MEDIA,
+		nil, 0,
+		nil, 0,
+		&bytesReturned,
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
-
-const (
-	FSCTL_LOCK_VOLUME     = uint32(0x90018)
-	FSCTL_DISMOUNT_VOLUME = uint32(0x90020)
-)
 
 func PrepareDrive(driveFile *os.File) error {
 	// I'm not 100% sure if this is correct, but locking and dismounting the drive seems to allow the write calls to succeed.
